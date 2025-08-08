b4e5f9cf02b2585b499758df74fd2dab9580ebdc
dzcazador <ctestaseca@hotmail.com>
2025-08-08 13:00:01 -0300

    Ajustes en Microservicios

--- its-gateway/src/invoice/invoice.controller.ts
+++ its-gateway/src/invoice/invoice.controller.ts
@@ -1,35 +1,56 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateInvoiceDto } from './dto/create-invoice.dto';
 import { UpdateInvoiceDto } from './dto/update-invoice.dto';
 import { MS_INVOICE } from 'src/common/constants';
+import { ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import { catchError } from 'rxjs';
+import { RpcResponse } from 'src/common/models/rpc.model';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Controller()
 export class InvoiceController {
   constructor(@Inject(MS_INVOICE) private readonly invoiceClient: ClientProxy) {}
-/*
-  @MessagePattern('createInvoice')
-  create(@Payload() createInvoiceDto: CreateInvoiceDto) {
-    return this.invoiceService.create(createInvoiceDto);
+
+  @Post()
+  @ApiOperation({ summary: 'Crear Factura' })
+  @ApiResponse({ status: 201, description: 'Factura Creada' })
+  @ApiBody({ type: CreateUserDto })
+  create(@Body() newInvoice: CreateUserDto) {
+
+    return this.invoiceClient.send({ invoices: 'create' }, { newInvoice }).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
 
-  @MessagePattern('findAllInvoice')
-  findAll() {
-    return this.invoiceService.findAll();
+  @Get(':id')
+  @ApiOperation({ summary: 'Obtener Factura por ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Factura encontrada' })
+  @ApiResponse({ status: 404, description: 'Factura no encontrada' })
+  async findById(@Param('id') id: number) {
+   return this.invoiceClient.send({ invoices: 'findOne' }, id).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
 
-  @MessagePattern('findOneInvoice')
-  findOne(@Payload() id: number) {
-    return this.invoiceService.findOne(id);
-  }
 
-  @MessagePattern('updateInvoice')
-  update(@Payload() updateInvoiceDto: UpdateInvoiceDto) {
-    return this.invoiceService.update(updateInvoiceDto.id, updateInvoiceDto);
+  @Get()
+  @ApiOperation({ summary: 'Obtener todos las Facturas' })
+  @ApiResponse({ status: 200, description: 'Facturas encontrados' })
+  @ApiResponse({ status: 404, description: 'Facturas no encontrados' })
+  async findAll() {
+   return this.invoiceClient.send({ invoices: 'findAll' }, {}).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
-
-  @MessagePattern('removeInvoice')
-  remove(@Payload() id: number) {
-    return this.invoiceService.remove(id);
-  } */
 }
--- its-gateway/src/product/product.controller.ts
+++ its-gateway/src/product/product.controller.ts
@@ -1,35 +1,57 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { MS_PRODUCT } from 'src/common/constants';
+import { ApiOperation, ApiResponse, ApiBody, ApiParam } from '@nestjs/swagger';
+import { catchError } from 'rxjs';
+import { RpcResponse } from 'src/common/models/rpc.model';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Controller()
 export class ProductController {
   constructor(@Inject(MS_PRODUCT) private readonly productClient: ClientProxy) {}
-/*
-  @MessagePattern('createProduct')
-  create(@Payload() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+
+  @Post()
+  @ApiOperation({ summary: 'Crear Producto' })
+  @ApiResponse({ status: 201, description: 'Producto Creado' })
+  @ApiBody({ type: CreateUserDto })
+  create(@Body() newProduct: CreateUserDto) {
+
+    return this.productClient.send({ produtcs: 'create' }, { newProduct }).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
 
-  @MessagePattern('findAllProduct')
-  findAll() {
-    return this.productService.findAll();
+  @Get(':id')
+  @ApiOperation({ summary: 'Obtener Producto por ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Producto encontrado' })
+  @ApiResponse({ status: 404, description: 'Producto no encontrado' })
+  async findById(@Param('id') id: number) {
+   return this.productClient.send({ produtcs: 'findOne' }, id).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
 
-  @MessagePattern('findOneProduct')
-  findOne(@Payload() id: number) {
-    return this.productService.findOne(id);
+
+  @Get()
+  @ApiOperation({ summary: 'Obtener todos los Productos' })
+  @ApiResponse({ status: 200, description: 'Productos encontrados' })
+  @ApiResponse({ status: 404, description: 'Productos no encontrados' })
+  async findAll() {
+   return this.productClient.send({ produtcs: 'findAll' }, {}).pipe(
+      catchError((rpcError: RpcResponse) => {
+        const { statusCode = 500, error } = rpcError;
+        throw new HttpException(error ?? rpcError, statusCode);
+      }),
+    );
   }
 
-  @MessagePattern('updateProduct')
-  update(@Payload() updateProductDto: UpdateProductDto) {
-    return this.productService.update(updateProductDto.id, updateProductDto);
-  }
-
-  @MessagePattern('removeProduct')
-  remove(@Payload() id: number) {
-    return this.productService.remove(id);
-  } */
 }
